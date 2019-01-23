ae59f2fb1ebb05edb4d6784eda8b263f4ba8ce4b
张建群 <zhang_jianqun7813@163.com>
2019-01-23 15:08:12 +0800

    Create LogsUserBrowseMapper.java

---
+++ gd-logs/gd-logs-user/src/main/java/com/gd/logsuser/mapper/LogsUserBrowseMapper.java
@@ -0,0 +1,16 @@
+package com.gd.logsuser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gd.logsuser.entity.LogsUserBrowse;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-01-22
+ */
+public interface LogsUserBrowseMapper extends BaseMapper<LogsUserBrowse> {
+
+}
