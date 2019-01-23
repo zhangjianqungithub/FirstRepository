2f8c0cc1c70256b58328b0edbd0419e29176a46b
张建群 <zhang_jianqun7813@163.com>
2019-01-23 15:07:49 +0800

    Create LogsUserLoginController.java

---
+++ gd-logs/gd-logs-user/src/main/java/com/gd/logsuser/controller/LogsUserLoginController.java
@@ -0,0 +1,20 @@
+package com.gd.logsuser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-01-22
+ */
+@RestController
+@RequestMapping("/gd/logs-user-login")
+public class LogsUserLoginController {
+
+
+}
