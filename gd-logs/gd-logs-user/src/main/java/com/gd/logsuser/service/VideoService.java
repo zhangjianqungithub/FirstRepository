d8b3c7787b2bfde751881acc8b2db592586e603d
张建群 <zhang_jianqun7813@163.com>
2019-01-23 15:09:34 +0800

    Create VideoService.java

---
+++ gd-logs/gd-logs-user/src/main/java/com/gd/logsuser/service/VideoService.java
@@ -0,0 +1,16 @@
+package com.gd.logsuser.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gd.logs.entity.Video;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-01-22
+ */
+public interface VideoService extends IService<Video> {
+
+}
