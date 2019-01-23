9969ef74e6613790bdc5285a1cb0ab3ac36917d0
张建群 <zhang_jianqun7813@163.com>
2019-01-23 15:09:28 +0800

    Create LogsUserRegistService.java

---
+++ gd-logs/gd-logs-user/src/main/java/com/gd/logsuser/service/LogsUserRegistService.java
@@ -0,0 +1,16 @@
+package com.gd.logsuser.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gd.logsuser.entity.LogsUserRegist;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-01-22
+ */
+public interface LogsUserRegistService extends IService<LogsUserRegist> {
+
+}
