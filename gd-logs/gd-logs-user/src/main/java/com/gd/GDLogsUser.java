090153c55077763774e313725d7fad7968a96f68
张建群 <zhang_jianqun7813@163.com>
2019-01-23 15:07:44 +0800

    Create GDLogsUser.java

---
+++ gd-logs/gd-logs-user/src/main/java/com/gd/GDLogsUser.java
@@ -0,0 +1,19 @@
+package com.gd;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//为Mapper文件生成代理对象
+@MapperScan("com.gd.logsuser.mapper")
+// 将服务注册到eureka中
+//@EnableEurekaClient
+//开启断路器机制
+//@EnableHystrix
+public class GDLogsUser {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GDLogsUser.class, args);
+	}
+}
